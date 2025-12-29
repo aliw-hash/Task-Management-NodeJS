e4d63274b86ae86c24ee7cacb6797711857d7276
aliw-hash <aliw24136@gmail.com>
2025-12-29 15:31:36 +0530

    added updateTask.provider.js

--- src/tasks/tasks.controller.js
+++ src/tasks/tasks.controller.js
@@ -1,6 +1,7 @@
 const {StatusCodes} = require("http-status-codes");
 const createTaskProvider = require("./providers/createTask.provider.js")
 const getTaskProvider = require("./providers/getTask.provider.js");
+const updateTaskProvider = require("./providers/updateTask.provider.js");
 
 async function handleGetTask(req,res){
   const tasks = await getTaskProvider(req,res);
@@ -12,10 +13,10 @@ async function handlePostTask(req,res){
   res.status(StatusCodes.CREATED).json(task);
 }
 
-function handlePatchTask(req,res){
-  res.send("Patch task controller");
+async function handlePatchTask(req,res){
+  const updatedTask = updateTaskProvider(req, res);
+  res.status(StatusCodes.OK).json(updatedTask);
 }
-
 function handleDeleteTask(req,res){
   res.send("DELETE task controller");
 }
